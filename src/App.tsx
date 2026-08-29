84048b1f61055caf247899f67794d172bc6ca50e
joelloo20-ai <joelloo20@gmail.com>
2026-08-29 22:58:42 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { GlobeExplorer } from './pages/GlobeExplorer';
 import { DestinationDetail } from './pages/DestinationDetail';
 
+
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<GlobeExplorer />} />
         <Route path="/destination/:key" element={<DestinationDetail />} />
@@ -13,4 +14,6 @@ function App() {
   );
 }
 
+
 export default App;
+
